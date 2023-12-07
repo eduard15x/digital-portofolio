708b16309d70aa21d080111c0197ba43c29fa7b6
Precup Eduard <precupeduard99@gmail.com>
2023-12-07 22:37:08 +0200

    Add required packages and fix problem

--- nestjs-server/src/app.module.ts
+++ nestjs-server/src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Project } from './project/entities/project.entity';
+import { ProjectModule } from './project/project.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { ImageController } from './image.controller';
+import { CloudinaryService } from './cloudinary.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'dbg',
+      port: 5432,
+      username: 'x',
+      password: 'x',
+      entities: [Project],
+      database: 'x',
+      synchronize: true,
+      ssl: true,
+    }),
+    ProjectModule,
+    MulterModule.register({
+      dest: './uploads',
+    }),
+  ],
+  controllers: [AppController, ImageController],
+  providers: [AppService, CloudinaryService],
 })
 export class AppModule {}
