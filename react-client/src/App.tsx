2b8de7302ebe484434707aac1882799ece705abf
Precup Eduard <precupeduard99@gmail.com>
2023-12-07 20:13:16 +0200

    Adding required packages and fix imports

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,7 +1,44 @@
-export const App: React.FC = () => {
+import { BrowserRouter, Routes, Route, Outlet, Navigate } from "react-router-dom";
+import ErrorPage from "./components/errorPage/ErrorPage";
+import Home from "./pages/Home";
+import { AdminPanel } from "./pages/AdminPanel";
+
+export default function App() {
+
+  const userState = {
+    userId: 0
+  }
+
+  const HomepageLayout = () => {
+    return (
+      <div>
+        <Outlet />
+        {/* <Footer /> */}
+      </div>
+    );
+  };
+
   return (
-    <div>
-      Hello
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+
+        {userState.userId === 0
+        ?
+          <Route path="/" element={<HomepageLayout />}>
+            <Route index element={<Home />} />
+            <Route path="admin" element={<AdminPanel />} />
+          </Route>
+        :
+          <>
+            <Route path="/" element={<Navigate to="/menu" />} />
+            <Route path="login" element={<Navigate to="/menu" />} />
+            <Route path="register" element={<Navigate to="/menu" />} />
+          </>
+        }
+
+        <Route path="*" element={<ErrorPage />} />
+
+      </Routes>
+    </BrowserRouter>
+  )
 }
